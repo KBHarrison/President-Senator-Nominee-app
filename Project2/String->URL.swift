4f68cf4968ae7dd1c847fd36f2c22db4fc8f2627
KyleHarrison <kylebharrison16@gmail.com>
2019-10-22 08:28:02 -0600

    finishing up the commit

---
+++ Project2/String->URL.swift
@@ -0,0 +1,20 @@
+//
+//  String->URL.swift
+//  Project2
+//
+//  Created by IS 543 on 10/21/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    var ToURL: URL {
+        if let urlValue = URL(string: self) {
+            return urlValue
+        }
+
+        fatalError("Unable to convert URL to string")
+    }
+}
